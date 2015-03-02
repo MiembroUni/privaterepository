c443d82f1415ba7f344963945db75d9c1c72d51a
MiembroUni <miembro.universitario@gmail.com>
2015-03-02 16:13:41 +0100

    primer commit

---
+++ ESCryptoSecureMegaBox/MainWindow.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ESCryptoSecureMegaBox
+{
+    partial class MW_Form_Ventana
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MW_Button_Encriptar = new System.Windows.Forms.Button();
+            this.MW_Button_Desencriptar = new System.Windows.Forms.Button();
+            this.MW_Label_Contraseña = new System.Windows.Forms.Label();
+            this.MW_TextBox_Contraseña = new System.Windows.Forms.TextBox();
+            this.MW_TreeView_Directorios = new System.Windows.Forms.TreeView();
+            this.MW_TextBox_Directorio = new System.Windows.Forms.TextBox();
+            this.MW_Button_Refresh = new System.Windows.Forms.Button();
+            this.MW_ProgressBar_Completado = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // MW_Button_Encriptar
+            // 
+            this.MW_Button_Encriptar.Location = new System.Drawing.Point(153, 67);
+            this.MW_Button_Encriptar.Name = "MW_Button_Encriptar";
+            this.MW_Button_Encriptar.Size = new System.Drawing.Size(75, 23);
+            this.MW_Button_Encriptar.TabIndex = 3;
+            this.MW_Button_Encriptar.Text = "Encriptar";
+            this.MW_Button_Encriptar.UseVisualStyleBackColor = true;
+            this.MW_Button_Encriptar.Click += new System.EventHandler(this.MW_Button_Encriptar_Click);
+            // 
+            // MW_Button_Desencriptar
+            // 
+            this.MW_Button_Desencriptar.Location = new System.Drawing.Point(153, 108);
+            this.MW_Button_Desencriptar.Name = "MW_Button_Desencriptar";
+            this.MW_Button_Desencriptar.Size = new System.Drawing.Size(75, 23);
+            this.MW_Button_Desencriptar.TabIndex = 4;
+            this.MW_Button_Desencriptar.Text = "Desencriptar";
+            this.MW_Button_Desencriptar.UseVisualStyleBackColor = true;
+            this.MW_Button_Desencriptar.Click += new System.EventHandler(this.MW_Button_Desencriptar_Click);
+            // 
+            // MW_Label_Contraseña
+            // 
+            this.MW_Label_Contraseña.AutoSize = true;
+            this.MW_Label_Contraseña.Location = new System.Drawing.Point(150, 13);
+            this.MW_Label_Contraseña.Name = "MW_Label_Contraseña";
+            this.MW_Label_Contraseña.Size = new System.Drawing.Size(61, 13);
+            this.MW_Label_Contraseña.TabIndex = 2;
+            this.MW_Label_Contraseña.Text = "Contraseña";
+            // 
+            // MW_TextBox_Contraseña
+            // 
+            this.MW_TextBox_Contraseña.Location = new System.Drawing.Point(153, 29);
+            this.MW_TextBox_Contraseña.Name = "MW_TextBox_Contraseña";
+            this.MW_TextBox_Contraseña.Size = new System.Drawing.Size(100, 20);
+            this.MW_TextBox_Contraseña.TabIndex = 2;
+            // 
+            // MW_TreeView_Directorios
+            // 
+            this.MW_TreeView_Directorios.Location = new System.Drawing.Point(13, 67);
+            this.MW_TreeView_Directorios.Name = "MW_TreeView_Directorios";
+            this.MW_TreeView_Directorios.Size = new System.Drawing.Size(131, 183);
+            this.MW_TreeView_Directorios.TabIndex = 0;
+            // 
+            // MW_TextBox_Directorio
+            // 
+            this.MW_TextBox_Directorio.Location = new System.Drawing.Point(13, 29);
+            this.MW_TextBox_Directorio.Name = "MW_TextBox_Directorio";
+            this.MW_TextBox_Directorio.Size = new System.Drawing.Size(102, 20);
+            this.MW_TextBox_Directorio.TabIndex = 1;
+            this.MW_TextBox_Directorio.Text = "C:\\";
+            this.MW_TextBox_Directorio.TextChanged += new System.EventHandler(this.MW_TextBox_Directorio_TextChanged);
+            // 
+            // MW_Button_Refresh
+            // 
+            this.MW_Button_Refresh.Location = new System.Drawing.Point(121, 26);
+            this.MW_Button_Refresh.Name = "MW_Button_Refresh";
+            this.MW_Button_Refresh.Size = new System.Drawing.Size(23, 23);
+            this.MW_Button_Refresh.TabIndex = 5;
+            this.MW_Button_Refresh.Text = "Refresh";
+            this.MW_Button_Refresh.UseVisualStyleBackColor = true;
+            this.MW_Button_Refresh.Click += new System.EventHandler(this.MW_Button_Refresh_Click);
+            // 
+            // MW_ProgressBar_Completado
+            // 
+            this.MW_ProgressBar_Completado.Location = new System.Drawing.Point(153, 226);
+            this.MW_ProgressBar_Completado.Name = "MW_ProgressBar_Completado";
+            this.MW_ProgressBar_Completado.Size = new System.Drawing.Size(119, 23);
+            this.MW_ProgressBar_Completado.TabIndex = 6;
+            // 
+            // MW_Form_Ventana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.MW_ProgressBar_Completado);
+            this.Controls.Add(this.MW_Button_Refresh);
+            this.Controls.Add(this.MW_TextBox_Directorio);
+            this.Controls.Add(this.MW_TreeView_Directorios);
+            this.Controls.Add(this.MW_TextBox_Contraseña);
+            this.Controls.Add(this.MW_Label_Contraseña);
+            this.Controls.Add(this.MW_Button_Desencriptar);
+            this.Controls.Add(this.MW_Button_Encriptar);
+            this.Name = "MW_Form_Ventana";
+            this.Text = "Ventana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MW_Button_Encriptar;
+        private System.Windows.Forms.Button MW_Button_Desencriptar;
+        private System.Windows.Forms.Label MW_Label_Contraseña;
+        private System.Windows.Forms.TextBox MW_TextBox_Contraseña;
+        private System.Windows.Forms.TreeView MW_TreeView_Directorios;
+        private System.Windows.Forms.TextBox MW_TextBox_Directorio;
+        private System.Windows.Forms.Button MW_Button_Refresh;
+        private System.Windows.Forms.ProgressBar MW_ProgressBar_Completado;
+    }
+}
+
---
+++ ESCryptoSecureMegaBox/MainWindow.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ESCryptoSecureMegaBox
+{
+    public partial class MW_Form_Ventana : Form
+    {
+        //P pssst salt
+        private static byte[] _salt = Encoding.ASCII.GetBytes("123456789");
+
+        public MW_Form_Ventana()
+        {
+            InitializeComponent();
+        }
+
+        private void MW_TextBox_Directorio_TextChanged(object sender, EventArgs e)
+        {
+            MW_Button_Refresh_Click(this, new EventArgs());
+        }
+
+        private void MW_Button_Encriptar_Click(object sender, EventArgs e)
+        {
+            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
+            String outputFile = inputFile + ".aesed";
+
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
+            byte[] file = File.ReadAllBytes(inputFile);
+
+            using (Aes aes= Aes.Create())
+            {
+                //Tamaño de clave? 256 / 8 
+                aes.Key = key.GetBytes(aes.KeySize / 8);
+                //Tamaño de bytes
+                aes.IV = key.GetBytes(16);
+                File.WriteAllBytes(outputFile, Cypher_Aes(file, aes.Key, aes.IV));
+
+                inputFile = outputFile;
+                outputFile = inputFile + "deaesed";
+                file = File.ReadAllBytes(inputFile);
+                File.WriteAllBytes(outputFile,DeCypher_Aes(file, aes.Key, aes.IV));
+
+            }
+            /*
+            //P CUIDAO CON LOS DIRECTORIOS ESTO ES PROVISIONAL
+            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
+            String outputFile = inputFile + ".aesed";
+
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text,_salt);
+            byte[] file = File.ReadAllBytes(inputFile);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key.GetBytes(aes.KeySize/8);
+                ICryptoTransform cypher = aes.CreateEncryptor(aes.Key, aes.IV);
+
+
+                // Create the streams used for encryption. 
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, cypher, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(file);
+                        }
+                        File.WriteAllBytes(outputFile, msEncrypt.ToArray());
+                    }
+                }                
+            }
+            */
+        }
+
+        private void MW_Button_Desencriptar_Click(object sender, EventArgs e)
+        {
+
+            /*
+            //P CUIDAO CON LOS DIRECTORIOS ESTO ES PROVISIONAL
+            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
+            String outputFile = inputFile + ".deaesed";
+
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
+            byte[] file = File.ReadAllBytes(inputFile);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key.GetBytes(aes.KeySize / 8);
+                ICryptoTransform decypher = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                // Create the streams used for encryption. 
+                using (MemoryStream msDecrypt = new MemoryStream(file))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decypher, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            File.WriteAllText(outputFile, srDecrypt.ReadToEnd());
+                        }
+                    }
+                }
+            }   
+            */
+        }
+
+        private void MW_Button_Refresh_Click(object sender, EventArgs e)
+        {
+            MW_TreeView_Directorios.Nodes.Clear();
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(@MW_TextBox_Directorio.Text);
+                if (directoryInfo.Exists)
+                {
+                    BuildTree(directoryInfo, MW_TreeView_Directorios.Nodes);
+                }
+            }
+            catch (Exception excepcion)
+            {
+                MessageBox.Show(excepcion.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+        {
+            TreeNode curNode = addInMe.Add(directoryInfo.Name);
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                curNode.Nodes.Add(file.FullName, file.Name);
+            }
+            foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+            {
+                BuildTree(subdir, curNode.Nodes);
+            }
+        }
+
+        private static byte[] Cypher_Aes(byte[] inputFile, byte[] Key, byte[] IV)
+        {
+            byte[] cyphed;
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                ICryptoTransform cypher = aes.CreateEncryptor(aes.Key, aes.IV);
+                
+                using (MemoryStream msCyphe = new MemoryStream())
+                {
+                    using (CryptoStream csCyphe = new CryptoStream(msCyphe, cypher, CryptoStreamMode.Write))
+                    {
+                        using (BinaryWriter swEncrypt = new BinaryWriter(csCyphe))
+                        {
+                            swEncrypt.Write(inputFile);
+                        }
+                        cyphed = msCyphe.ToArray();
+                    }
+                }
+            }
+
+            return cyphed;
+
+        }
+
+        private static byte[] DeCypher_Aes(byte[] cyphedText, byte[] Key, byte[] IV)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                ICryptoTransform decypher = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream msDecyphe = new MemoryStream(cyphedText))
+                {
+                    using (CryptoStream csDecyphe = new CryptoStream(msDecyphe, decypher, CryptoStreamMode.Read))
+                    {
+                        using (BinaryReader srDecyphe = new BinaryReader(csDecyphe)) 
+                        {
+                            MemoryStream outputFile = new MemoryStream();
+
+                            const int buffsize = 4096;
+                            byte[] buffer = new byte[buffsize];
+                            int count;
+
+                            while ((count = srDecyphe.Read(buffer, 0, buffer.Length)) != 0)
+                                outputFile.Write(buffer, 0, count);
+
+                            return outputFile.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
