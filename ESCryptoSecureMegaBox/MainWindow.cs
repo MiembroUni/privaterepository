cfbf263d2b36807def3e0c2c7d5b6bc0849ee1b1
MiembroUni <miembro.universitario@gmail.com>
2015-03-09 11:04:45 +0100

    El boton de encriptar encripta y el de desencriptar desencripta, ya no se apilan extensiones

--- ESCryptoSecureMegaBox/MainWindow.cs
+++ ESCryptoSecureMegaBox/MainWindow.cs
@@ -11,12 +11,16 @@ using System.Windows.Forms;
 using System.IO;
 using System.Security.Cryptography;
 
+//P Esto se quita al final
+using System.Diagnostics;
+
 namespace ESCryptoSecureMegaBox
 {
     public partial class MW_Form_Ventana : Form
     {
         //P pssst salt
         private static byte[] _salt = Encoding.ASCII.GetBytes("123456789");
+        private static int Aes_KeySize = /*aes.KeySize / 8 */32;
 
         public MW_Form_Ventana()
         {
@@ -24,32 +28,39 @@ namespace ESCryptoSecureMegaBox
         }
 
         private void MW_TextBox_Directorio_TextChanged(object sender, EventArgs e)
-        {
+        {   
             MW_Button_Refresh_Click(this, new EventArgs());
         }
 
         private void MW_Button_Encriptar_Click(object sender, EventArgs e)
         {
             String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
-            String outputFile = inputFile + ".aesed";
+            String outputFile = inputFile + ".aesed";            
 
             Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
-            byte[] file = File.ReadAllBytes(inputFile);
 
             using (Aes aes= Aes.Create())
             {
+                byte[] file = File.ReadAllBytes(inputFile);
                 //Tamaño de clave? 256 / 8 
-                aes.Key = key.GetBytes(aes.KeySize / 8);
+                aes.Key = key.GetBytes(Aes_KeySize);
                 //Tamaño de bytes
-                aes.IV = key.GetBytes(16);
-                File.WriteAllBytes(outputFile, Cypher_Aes(file, aes.Key, aes.IV));
+                //aes.IV = key.GetBytes(16);
+                
+                byte[] Cyphed = Cypher_Aes(file, aes.Key, aes.IV);
+                //Array.Resize(Cyphed,)
 
-                inputFile = outputFile;
-                outputFile = inputFile + "deaesed";
-                file = File.ReadAllBytes(inputFile);
-                File.WriteAllBytes(outputFile,DeCypher_Aes(file, aes.Key, aes.IV));
+                //Tamaño de IV, IV, Texto Cifrado
+                //32 bits, IV.length, resto
+                byte[] headIV = new byte[aes.IV.Length+Cyphed.Length+BitConverter.GetBytes(aes.IV.Length).Length];
+                
+                Array.Copy(BitConverter.GetBytes(aes.IV.Length),0,headIV,0,BitConverter.GetBytes(aes.IV.Length).Length);
+                Array.Copy(aes.IV,0,headIV,BitConverter.GetBytes(aes.IV.Length).Length,aes.IV.Length);
+                Array.Copy(Cyphed,0,headIV,BitConverter.GetBytes(aes.IV.Length).Length+aes.IV.Length,Cyphed.Length);
 
+                File.WriteAllBytes(outputFile, headIV);
             }
+
             /*
             //P CUIDAO CON LOS DIRECTORIOS ESTO ES PROVISIONAL
             String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
@@ -83,7 +94,35 @@ namespace ESCryptoSecureMegaBox
 
         private void MW_Button_Desencriptar_Click(object sender, EventArgs e)
         {
+            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
+            //P extension
+            String outputFile = System.IO.Path.ChangeExtension(inputFile, null) + "1";
 
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
+
+            //byte[] file = File.ReadAllBytes(inputFile);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key.GetBytes(Aes_KeySize);
+                using (BinaryReader bRead = new BinaryReader(File.OpenRead(inputFile)))
+                {
+                    int IVlength = bRead.ReadInt32();                                        
+                    aes.IV = bRead.ReadBytes(IVlength);                    
+                    byte[] file;
+
+                    const int bufferSize = 4096;
+                    using (MemoryStream mStream = new MemoryStream())
+                    {
+                        byte[] buffer = new byte[bufferSize];
+                        int count;
+                        while ((count = bRead.Read(buffer, 0, buffer.Length)) != 0)
+                            mStream.Write(buffer, 0, count);
+                        file= mStream.ToArray();
+                    }
+                    File.WriteAllBytes(outputFile, DeCypher_Aes(file, aes.Key, aes.IV));
+                }
+            }
             /*
             //P CUIDAO CON LOS DIRECTORIOS ESTO ES PROVISIONAL
             String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
@@ -189,16 +228,18 @@ namespace ESCryptoSecureMegaBox
                     {
                         using (BinaryReader srDecyphe = new BinaryReader(csDecyphe)) 
                         {
-                            MemoryStream outputFile = new MemoryStream();
+                            using (MemoryStream outputFile = new MemoryStream())
+                            {
 
-                            const int buffsize = 4096;
-                            byte[] buffer = new byte[buffsize];
-                            int count;
+                                const int buffsize = 4096;
+                                byte[] buffer = new byte[buffsize];
+                                int count;
 
-                            while ((count = srDecyphe.Read(buffer, 0, buffer.Length)) != 0)
-                                outputFile.Write(buffer, 0, count);
+                                while ((count = srDecyphe.Read(buffer, 0, buffer.Length)) != 0)
+                                    outputFile.Write(buffer, 0, count);
 
-                            return outputFile.ToArray();
+                                return outputFile.ToArray();
+                            }
                         }
                     }
                 }
