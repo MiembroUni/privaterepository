8ab71dcc3c018a60a71992c7890dafdfa9b5e972
MiembroUni <miembro.universitario@gmail.com>
2015-04-20 10:55:20 +0200

    +Encripta,+Desencripta;+Refactorizado;-Drag&Drop;-Iconos;

--- ESCryptoSecureMegaBox/MainWindow.Designer.cs
+++ ESCryptoSecureMegaBox/MainWindow.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MW_Form_Ventana));
             this.MW_Button_Encriptar = new System.Windows.Forms.Button();
             this.MW_Button_Desencriptar = new System.Windows.Forms.Button();
             this.MW_Label_Contraseña = new System.Windows.Forms.Label();
             this.MW_TextBox_Contraseña = new System.Windows.Forms.TextBox();
             this.MW_TreeView_Directorios = new System.Windows.Forms.TreeView();
-            this.MW_TextBox_Directorio = new System.Windows.Forms.TextBox();
-            this.MW_Button_Refresh = new System.Windows.Forms.Button();
+            this.MW_ImageList_IconosDirectorios = new System.Windows.Forms.ImageList(this.components);
             this.MW_ProgressBar_Completado = new System.Windows.Forms.ProgressBar();
+            this.MW_SplitContainer_Explorador = new System.Windows.Forms.SplitContainer();
+            this.MW_ListView_Archivos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.MW_SplitContainer_Explorador)).BeginInit();
+            this.MW_SplitContainer_Explorador.Panel1.SuspendLayout();
+            this.MW_SplitContainer_Explorador.Panel2.SuspendLayout();
+            this.MW_SplitContainer_Explorador.SuspendLayout();
             this.SuspendLayout();
             // 
             // MW_Button_Encriptar
@@ -73,32 +83,26 @@
             this.MW_TextBox_Contraseña.Name = "MW_TextBox_Contraseña";
             this.MW_TextBox_Contraseña.Size = new System.Drawing.Size(100, 20);
             this.MW_TextBox_Contraseña.TabIndex = 2;
+            this.MW_TextBox_Contraseña.TextChanged += new System.EventHandler(this.MW_TextBox_Contraseña_TextChanged);
             // 
             // MW_TreeView_Directorios
             // 
-            this.MW_TreeView_Directorios.Location = new System.Drawing.Point(13, 67);
+            this.MW_TreeView_Directorios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MW_TreeView_Directorios.ImageIndex = 0;
+            this.MW_TreeView_Directorios.ImageList = this.MW_ImageList_IconosDirectorios;
+            this.MW_TreeView_Directorios.Location = new System.Drawing.Point(0, 0);
             this.MW_TreeView_Directorios.Name = "MW_TreeView_Directorios";
-            this.MW_TreeView_Directorios.Size = new System.Drawing.Size(131, 183);
+            this.MW_TreeView_Directorios.SelectedImageIndex = 0;
+            this.MW_TreeView_Directorios.Size = new System.Drawing.Size(116, 262);
             this.MW_TreeView_Directorios.TabIndex = 0;
+            this.MW_TreeView_Directorios.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.MW_TreeView_Directorios_NodeMouseClick);
             // 
-            // MW_TextBox_Directorio
+            // MW_ImageList_IconosDirectorios
             // 
-            this.MW_TextBox_Directorio.Location = new System.Drawing.Point(13, 29);
-            this.MW_TextBox_Directorio.Name = "MW_TextBox_Directorio";
-            this.MW_TextBox_Directorio.Size = new System.Drawing.Size(102, 20);
-            this.MW_TextBox_Directorio.TabIndex = 1;
-            this.MW_TextBox_Directorio.Text = "C:\\";
-            this.MW_TextBox_Directorio.TextChanged += new System.EventHandler(this.MW_TextBox_Directorio_TextChanged);
-            // 
-            // MW_Button_Refresh
-            // 
-            this.MW_Button_Refresh.Location = new System.Drawing.Point(121, 26);
-            this.MW_Button_Refresh.Name = "MW_Button_Refresh";
-            this.MW_Button_Refresh.Size = new System.Drawing.Size(23, 23);
-            this.MW_Button_Refresh.TabIndex = 5;
-            this.MW_Button_Refresh.Text = "Refresh";
-            this.MW_Button_Refresh.UseVisualStyleBackColor = true;
-            this.MW_Button_Refresh.Click += new System.EventHandler(this.MW_Button_Refresh_Click);
+            this.MW_ImageList_IconosDirectorios.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("MW_ImageList_IconosDirectorios.ImageStream")));
+            this.MW_ImageList_IconosDirectorios.TransparentColor = System.Drawing.Color.Transparent;
+            this.MW_ImageList_IconosDirectorios.Images.SetKeyName(0, "stock_lock.png");
+            this.MW_ImageList_IconosDirectorios.Images.SetKeyName(1, "stock_lock_open.png");
             // 
             // MW_ProgressBar_Completado
             // 
@@ -107,21 +111,68 @@
             this.MW_ProgressBar_Completado.Size = new System.Drawing.Size(119, 23);
             this.MW_ProgressBar_Completado.TabIndex = 6;
             // 
+            // MW_SplitContainer_Explorador
+            // 
+            this.MW_SplitContainer_Explorador.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MW_SplitContainer_Explorador.Location = new System.Drawing.Point(292, 0);
+            this.MW_SplitContainer_Explorador.Name = "MW_SplitContainer_Explorador";
+            // 
+            // MW_SplitContainer_Explorador.Panel1
+            // 
+            this.MW_SplitContainer_Explorador.Panel1.Controls.Add(this.MW_TreeView_Directorios);
+            // 
+            // MW_SplitContainer_Explorador.Panel2
+            // 
+            this.MW_SplitContainer_Explorador.Panel2.Controls.Add(this.MW_ListView_Archivos);
+            this.MW_SplitContainer_Explorador.Size = new System.Drawing.Size(348, 262);
+            this.MW_SplitContainer_Explorador.SplitterDistance = 116;
+            this.MW_SplitContainer_Explorador.TabIndex = 7;
+            // 
+            // MW_ListView_Archivos
+            // 
+            this.MW_ListView_Archivos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.MW_ListView_Archivos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MW_ListView_Archivos.Location = new System.Drawing.Point(0, 0);
+            this.MW_ListView_Archivos.Name = "MW_ListView_Archivos";
+            this.MW_ListView_Archivos.Size = new System.Drawing.Size(228, 262);
+            this.MW_ListView_Archivos.SmallImageList = this.MW_ImageList_IconosDirectorios;
+            this.MW_ListView_Archivos.TabIndex = 0;
+            this.MW_ListView_Archivos.UseCompatibleStateImageBehavior = false;
+            this.MW_ListView_Archivos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nombre";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tipo";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Fecha de modificación";
+            // 
             // MW_Form_Ventana
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(640, 262);
+            this.Controls.Add(this.MW_SplitContainer_Explorador);
             this.Controls.Add(this.MW_ProgressBar_Completado);
-            this.Controls.Add(this.MW_Button_Refresh);
-            this.Controls.Add(this.MW_TextBox_Directorio);
-            this.Controls.Add(this.MW_TreeView_Directorios);
             this.Controls.Add(this.MW_TextBox_Contraseña);
             this.Controls.Add(this.MW_Label_Contraseña);
             this.Controls.Add(this.MW_Button_Desencriptar);
             this.Controls.Add(this.MW_Button_Encriptar);
             this.Name = "MW_Form_Ventana";
             this.Text = "Ventana";
+            this.MW_SplitContainer_Explorador.Panel1.ResumeLayout(false);
+            this.MW_SplitContainer_Explorador.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MW_SplitContainer_Explorador)).EndInit();
+            this.MW_SplitContainer_Explorador.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,9 +185,13 @@
         private System.Windows.Forms.Label MW_Label_Contraseña;
         private System.Windows.Forms.TextBox MW_TextBox_Contraseña;
         private System.Windows.Forms.TreeView MW_TreeView_Directorios;
-        private System.Windows.Forms.TextBox MW_TextBox_Directorio;
-        private System.Windows.Forms.Button MW_Button_Refresh;
         private System.Windows.Forms.ProgressBar MW_ProgressBar_Completado;
+        private System.Windows.Forms.SplitContainer MW_SplitContainer_Explorador;
+        private System.Windows.Forms.ImageList MW_ImageList_IconosDirectorios;
+        private System.Windows.Forms.ListView MW_ListView_Archivos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- ESCryptoSecureMegaBox/MainWindow.cs
+++ ESCryptoSecureMegaBox/MainWindow.cs
@@ -11,8 +11,15 @@ using System.Windows.Forms;
 using System.IO;
 using System.Security.Cryptography;
 
+using System.Text.RegularExpressions;
+
 //P Esto se quita al final
 using System.Diagnostics;
+
+//@todo
+//refactorizar los botones de encriptAR,Desentriptar
+//drag & drop
+
 
 namespace ESCryptoSecureMegaBox
 {
@@ -20,85 +27,98 @@ namespace ESCryptoSecureMegaBox
     {
         //P pssst salt
         private static byte[] _salt = Encoding.ASCII.GetBytes("123456789");
-        private static int Aes_KeySize = /*aes.KeySize / 8 */32;
+        private static int Aes_KeySize = /*aes.KeySize / 8 , 256 / 8*/32;
+
+        private static string initialRoute = (@"C:\Test");
+        private static string inputPath;
+        //Pout este e el der dropbos
+        private static string outputPath;
+
+        private static string password;
 
         public MW_Form_Ventana()
         {
             InitializeComponent();
+            PopulateTreeView();
         }
 
-        private void MW_TextBox_Directorio_TextChanged(object sender, EventArgs e)
-        {   
-            MW_Button_Refresh_Click(this, new EventArgs());
+        private void MW_TextBox_Contraseña_TextChanged(object sender, EventArgs e)
+        {
+            password = MW_TextBox_Contraseña.Text;
         }
 
         private void MW_Button_Encriptar_Click(object sender, EventArgs e)
         {
-            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
-            String outputFile = inputFile + ".aesed";            
+            ListView.SelectedListViewItemCollection inputCollection = MW_ListView_Archivos.SelectedItems;
 
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
+            String inputFile = Regex.Match(initialRoute, "^[a-zA-Z]") + ":" + @"\" + sub + @"\";
+            String outputFile;
 
-            using (Aes aes= Aes.Create())
+            foreach (ListViewItem item in inputCollection)
+            {
+                inputFile += item.Text;
+                outputFile = inputFile + ".aesed";
+
+                Cifrar(inputFile, outputFile);
+            }
+        }
+
+        private void MW_Button_Desencriptar_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection inputCollection = MW_ListView_Archivos.SelectedItems;
+
+            String inputFile = Regex.Match(initialRoute, "^[a-zA-Z]") + ":" + @"\" + sub + @"\";
+            //P extension
+            String outputFile;
+
+            foreach (ListViewItem item in inputCollection)
+            {
+                inputFile += item.Text;
+                outputFile = System.IO.Path.ChangeExtension(inputFile, null) + "1";
+
+                Descifrar(inputFile, outputFile);
+            }
+        }
+
+        private void MW_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private static void Cifrar(string inputFile, string outputFile)
+        {
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, _salt);
+
+            using (Aes aes = Aes.Create())
             {
                 byte[] file = File.ReadAllBytes(inputFile);
                 //Tamaño de clave? 256 / 8 
                 aes.Key = key.GetBytes(Aes_KeySize);
                 //Tamaño de bytes
                 //aes.IV = key.GetBytes(16);
-                
+
                 byte[] Cyphed = Cypher_Aes(file, aes.Key, aes.IV);
                 //Array.Resize(Cyphed,)
 
                 //Tamaño de IV, IV, Texto Cifrado
                 //32 bits, IV.length, resto
-                byte[] headIV = new byte[aes.IV.Length+Cyphed.Length+BitConverter.GetBytes(aes.IV.Length).Length];
-                
-                Array.Copy(BitConverter.GetBytes(aes.IV.Length),0,headIV,0,BitConverter.GetBytes(aes.IV.Length).Length);
-                Array.Copy(aes.IV,0,headIV,BitConverter.GetBytes(aes.IV.Length).Length,aes.IV.Length);
-                Array.Copy(Cyphed,0,headIV,BitConverter.GetBytes(aes.IV.Length).Length+aes.IV.Length,Cyphed.Length);
+                byte[] headIV = new byte[aes.IV.Length + Cyphed.Length + BitConverter.GetBytes(aes.IV.Length).Length];
+
+                Array.Copy(BitConverter.GetBytes(aes.IV.Length), 0, headIV, 0, BitConverter.GetBytes(aes.IV.Length).Length);
+                Array.Copy(aes.IV, 0, headIV, BitConverter.GetBytes(aes.IV.Length).Length, aes.IV.Length);
+                Array.Copy(Cyphed, 0, headIV, BitConverter.GetBytes(aes.IV.Length).Length + aes.IV.Length, Cyphed.Length);
 
                 File.WriteAllBytes(outputFile, headIV);
             }
-
-            /*
-            //P CUIDAO CON LOS DIRECTORIOS ESTO ES PROVISIONAL
-            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
-            String outputFile = inputFile + ".aesed";
-
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text,_salt);
-            byte[] file = File.ReadAllBytes(inputFile);
-
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = key.GetBytes(aes.KeySize/8);
-                ICryptoTransform cypher = aes.CreateEncryptor(aes.Key, aes.IV);
-
-
-                // Create the streams used for encryption. 
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, cypher, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            //Write all data to the stream.
-                            swEncrypt.Write(file);
-                        }
-                        File.WriteAllBytes(outputFile, msEncrypt.ToArray());
-                    }
-                }                
-            }
-            */
         }
 
-        private void MW_Button_Desencriptar_Click(object sender, EventArgs e)
+        private static void Descifrar(string inputFile, string outputFile)
         {
-            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
-            //P extension
-            String outputFile = System.IO.Path.ChangeExtension(inputFile, null) + "1";
-
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, _salt);
 
             //byte[] file = File.ReadAllBytes(inputFile);
 
@@ -107,8 +127,8 @@ namespace ESCryptoSecureMegaBox
                 aes.Key = key.GetBytes(Aes_KeySize);
                 using (BinaryReader bRead = new BinaryReader(File.OpenRead(inputFile)))
                 {
-                    int IVlength = bRead.ReadInt32();                                        
-                    aes.IV = bRead.ReadBytes(IVlength);                    
+                    int IVlength = bRead.ReadInt32();
+                    aes.IV = bRead.ReadBytes(IVlength);
                     byte[] file;
 
                     const int bufferSize = 4096;
@@ -118,69 +138,10 @@ namespace ESCryptoSecureMegaBox
                         int count;
                         while ((count = bRead.Read(buffer, 0, buffer.Length)) != 0)
                             mStream.Write(buffer, 0, count);
-                        file= mStream.ToArray();
+                        file = mStream.ToArray();
                     }
                     File.WriteAllBytes(outputFile, DeCypher_Aes(file, aes.Key, aes.IV));
                 }
-            }
-            /*
-            //P CUIDAO CON LOS DIRECTORIOS ESTO ES PROVISIONAL
-            String inputFile = MW_TreeView_Directorios.SelectedNode.Name;
-            String outputFile = inputFile + ".deaesed";
-
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(MW_TextBox_Contraseña.Text, _salt);
-            byte[] file = File.ReadAllBytes(inputFile);
-
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = key.GetBytes(aes.KeySize / 8);
-                ICryptoTransform decypher = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                // Create the streams used for encryption. 
-                using (MemoryStream msDecrypt = new MemoryStream(file))
-                {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decypher, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
-                            File.WriteAllText(outputFile, srDecrypt.ReadToEnd());
-                        }
-                    }
-                }
-            }   
-            */
-        }
-
-        private void MW_Button_Refresh_Click(object sender, EventArgs e)
-        {
-            MW_TreeView_Directorios.Nodes.Clear();
-            try
-            {
-                DirectoryInfo directoryInfo = new DirectoryInfo(@MW_TextBox_Directorio.Text);
-                if (directoryInfo.Exists)
-                {
-                    BuildTree(directoryInfo, MW_TreeView_Directorios.Nodes);
-                }
-            }
-            catch (Exception excepcion)
-            {
-                MessageBox.Show(excepcion.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
-        {
-            TreeNode curNode = addInMe.Add(directoryInfo.Name);
-
-            foreach (FileInfo file in directoryInfo.GetFiles())
-            {
-                curNode.Nodes.Add(file.FullName, file.Name);
-            }
-            foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
-            {
-                BuildTree(subdir, curNode.Nodes);
             }
         }
 
@@ -243,6 +204,120 @@ namespace ESCryptoSecureMegaBox
                         }
                     }
                 }
+            }
+        }
+
+        private string uplevel; // parent path
+        private static string sub;
+
+        private string CheckParent(TreeNode child)
+        {
+            if (child.Parent != null)
+            {
+                TreeNode temp = child.Parent;
+                uplevel = this.CheckParent(temp) + @"\" + child.Text;
+            }
+            else
+            {
+                uplevel = child.Text;
+            }
+            return uplevel;
+        }
+
+
+
+        //Ptreessss
+        private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+        {
+            TreeNode curNode = addInMe.Add(directoryInfo.Name);
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                curNode.Nodes.Add(file.FullName, file.Name);
+            }
+            foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+            {
+                BuildTree(subdir, curNode.Nodes);
+            }
+        }
+
+        void MW_TreeView_Directorios_NodeMouseClick(object sender,
+            TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+
+            uplevel = "";
+            sub = this.CheckParent(newSelected);
+            inputPath = sub;
+
+            MW_ListView_Archivos.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    {new ListViewItem.ListViewSubItem(item, "Directory"), 
+                     new ListViewItem.ListViewSubItem(item, 
+						dir.LastAccessTime.ToShortDateString())};
+                item.SubItems.AddRange(subItems);
+                MW_ListView_Archivos.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "File"), 
+                     new ListViewItem.ListViewSubItem(item, 
+						file.LastAccessTime.ToShortDateString())};
+
+                item.SubItems.AddRange(subItems);
+                MW_ListView_Archivos.Items.Add(item);
+            }
+
+            MW_ListView_Archivos.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+        
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+
+            try
+            {
+                DirectoryInfo info = new DirectoryInfo(initialRoute);
+                if (info.Exists)
+                {
+                    rootNode = new TreeNode(info.Name);
+                    rootNode.Tag = info;
+                    GetDirectories(info.GetDirectories(), rootNode);
+                    MW_TreeView_Directorios.Nodes.Add(rootNode);
+                }
+
+            }
+            catch (UnauthorizedAccessException excepcion)
+            {
+                MessageBox.Show(excepcion.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+       
+        private void GetDirectories(DirectoryInfo[] subDirs,
+            TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
             }
         }
     }
